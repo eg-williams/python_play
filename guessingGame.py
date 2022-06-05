f7e7fbc8ccc9db973361c2a8535d2845a9728b01
Emyr Williams <egwilliams2002@googlemail.com>
2022-06-05 12:54:04 +0100

    added guessing game, to help understand input and output

---
+++ guessingGame.py
@@ -0,0 +1,15 @@
+from random import randrange
+guess = randrange(0,21,2)
+numGuesses = 0
+while(numGuesses != 5):
+    currentGuess = int(input("Guess the number between 1 and 20:"))
+    if currentGuess == guess:
+        print("You are correct")
+        print("The answer was %s" % guess)
+        break
+    else:
+        remaining = 5 - numGuesses
+        print("You have %s guesses remaining" % (remaining - 1))
+        numGuesses += 1
+
+print("The answer was %s" %guess)
