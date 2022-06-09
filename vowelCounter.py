6775ced8b314791effa946ac33abe91f87566796
Emyr Williams <egwilliams2002@googlemail.com>
2022-06-09 21:42:48 +0100

    tidying up code directory. didn't want to put MIT CS600 problem sets in public repo

--- vowelCounter.py
+++
@@ -1,8 +0,0 @@
-numOfVowels = 0
-s = 'azcbobobegghakl'
-
-for char in s:
-    if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
-        numOfVowels += 1
-
-print("Number of vowels: " + str(numOfVowels))
